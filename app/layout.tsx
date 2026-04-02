7c59a818a76e88de698bdc31dae9b3b102fa621f
takahiro inaba <inabat@dlfl.jp>
2026-04-02 10:48:28 +0900

    update: site settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: "サッカー等スポーツのユニフォーム　お役立ちニュース！",
-  description: "ユニフォーム担当者が賢くなれる場所。世界のユニフォームニュースを",
+  title: "サッカー、バスケ、バレー、陸上、スポーツのユニフォーム　お役立ちニュース！",
+  description: "ユニフォーム担当者に役立つ場所。世界のユニフォームニュースを",
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <header>
           <div className="container">
-            <a href="/" className="site-title">チームマックスに役立つニュース（サイトタイトル）</a>
+            <a href="/" className="site-title">チームユニフォームTeammaxニュース</a>
             <p className="site-desc">
               メール担当者が賢くなれる場所 ／{' '}
               <a href="https://www.meruhaikun.com/" target="_blank" rel="noopener noreferrer" className="service-link">
